11c136033d3c1486a96765669161602188577e2e
QfEifeIfyU <2034098113@qq.com>
2020-02-23 21:05:31 +0800

    结构化的网络消息封装--客户端

--- EasyTcpClient/client.cpp
+++ EasyTcpClient/client.cpp
@@ -2,12 +2,46 @@
 #define WIN32_LEAN_AND_MEAN			// 避免windows库和WinSock2库的宏重定义
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 
+enum CMD {
+	_LOGIN,
+	_LOGOUT,
+	_ERROR
+};
+
+// 传输结构化数据格式
+struct DataHeader {
+	short _dataLength;
+	CMD _cmd;		// 命令
+};
+
+struct Login {
+	char _userName[32];
+	char _passwd[32];
+};
+
+struct LoginResult {
+	bool _result;
+};
+
+struct Logout {
+	char _userName[32];
+};
+
+struct LogoutResult {
+	bool _result;
+};
+
+struct Response {
+	bool _result;
+};
+
 #include <windows.h>
 #include <WinSock2.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define PORT 4567
+
 
 //#pragma comment(lib, "ws2_32.lib")     // 常规->输入->附加依赖项中添加库名称
 using namespace std;
@@ -40,28 +74,45 @@ int main() {
 			// 3. 输入请求
 			char request[128] = {};
 			printf("请输入请求: ");
-			
+			fflush(0);
 			scanf("%s", request);
 
-			// 4. 使用 strcmp 处理请求
-			if (strcmp(request, "exit") == 0) {
-				break;
+			// 4. 使用 strcmp 处理请求，并将数据包发送给服务器
+			if (strcmp(request, "login") == 0) {
+				DataHeader head = { sizeof(Login), _LOGIN };
+				Login login = {"lyb", "123456"};
+				// 发送包头+包体
+				send(sockfd, reinterpret_cast<const char*>(&head), sizeof(DataHeader), 0);
+				send(sockfd, reinterpret_cast<const char*>(&login), sizeof(Login), 0);
 			}
-
-			// 5. 向服务器发送请求
-			send(sockfd, request, strlen(request) + 1, 0);
-
-			// 6.recv 接收服务器数据
-#define BUF_SIZE 256
-			char recv_buf[BUF_SIZE] = {};
-			int size_recv = recv(sockfd, recv_buf, BUF_SIZE, 0);
-			if (size_recv > 0) {
-				printf("\tresponse：%s\n", recv_buf);
+			else if (strcmp(request, "logout") == 0) {
+				DataHeader head = { sizeof(Logout), _LOGOUT };
+				Logout logout = { "lyb" };
+				// 发送包头+包体
+				send(sockfd, reinterpret_cast<const char*>(&head), sizeof(DataHeader), 0);
+				send(sockfd, reinterpret_cast<const char*>(&logout), sizeof(Logout), 0);
 			}
 			else {
-				printf("服务器端停止接收数据...\n");
+				if (strcmp(request, "exit") == 0) 
+					printf("exit, 任务结束.\n");
+				else
+					printf("error, 不支持的命令.\n");
 				break;
 			}
+			// 接收服务器的响应数据
+			DataHeader response_head = {};
+			Response response = {};
+			recv(sockfd, reinterpret_cast<char*>(&response_head), sizeof(DataHeader), 0);
+			recv(sockfd, reinterpret_cast<char*>(&response), sizeof(Response), 0);
+#ifndef Debug
+			printf("\t ##log result: %d->", response._result);
+			if (response._result == true) {
+				printf("成功\n");
+			}
+			else {
+				printf("失败\n");
+			}
+#endif
 		}		
 	}
 	
