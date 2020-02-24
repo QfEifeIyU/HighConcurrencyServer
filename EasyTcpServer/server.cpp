44ce4aeec4d6d6220dc30d629e2a6caf4cda55ed
QfEifeIfyU <2034098113@qq.com>
2020-02-24 13:47:35 +0800

    服务器2.0，添加select模型-非阻塞版本

--- EasyTcpServer/server.cpp
+++ EasyTcpServer/server.cpp
@@ -24,15 +24,6 @@ struct Login :public DataHeader{
 	char _passwd[32];
 };
 
-struct LoginResult :public DataHeader{
-	LoginResult() {
-		this->_cmd = _LOGIN_RESULT;
-		this->_dataLength = sizeof(LoginResult);
-		this->_result = false;
-	}
-	bool _result;
-};
-
 struct Logout :public DataHeader {
 	Logout() {
 		this->_cmd = _LOGOUT;
@@ -40,7 +31,15 @@ struct Logout :public DataHeader {
 	}
 	char _userName[32];
 };
-
+#ifdef Debug
+struct LoginResult :public DataHeader {
+	LoginResult() {
+		this->_cmd = _LOGIN_RESULT;
+		this->_dataLength = sizeof(LoginResult);
+		this->_result = false;
+	}
+	bool _result;
+};
 struct LogoutResult :public DataHeader{
 	LogoutResult() {
 		this->_cmd = _LOGOUT_RESULT;
@@ -49,6 +48,7 @@ struct LogoutResult :public DataHeader{
 	}
 	bool _result;
 };
+#endif
 
 struct Response : public DataHeader{
 	Response() {
@@ -63,10 +63,68 @@ struct Response : public DataHeader{
 #include <WinSock2.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <vector>
 //#pragma comment(lib, "ws2_32.lib")     // 链接器->输入->附加依赖项中添加库名称
 #define PORT 4567
 using namespace std;
+
+// 添加select模型
+vector<SOCKET> g_TalkWithCli_fds;
+
+
+bool Proc(SOCKET cli_sockfd) {
+	// 5.recv 接收客户端请求数据包
+	DataHeader request_head = {};	// 接收请求头信息
+	int recv_len = recv(cli_sockfd, reinterpret_cast<char*>(&request_head), sizeof(DataHeader), 0);
+	if (recv_len <= 0) {
+		printf("接收数据失败，客户端退出，任务结束\n");
+		return false;
+	}
+
+	// 6.处理客户端请求，解析信息
+	Response response = {};				// 响应
+	switch (request_head._cmd) {
+		case _LOGIN: {
+			// 登录->返回登录信息
+			Login login = {};	// 接收包体
+			recv(cli_sockfd, reinterpret_cast<char*>(&login) + sizeof(DataHeader),
+				sizeof(Login) - sizeof(DataHeader), 0);
+	#ifndef Debug
+			printf("包头信息：$cmd: %d, $lenght: %d\n", request_head._cmd, request_head._dataLength);
+			printf("\t$%s, $%s已上线.\n", login._userName, login._passwd);
+	#endif
+			// here is 判断登录信息，构造响应包
+			response._cmd = _LOGIN_RESULT;
+			response._result = true;
+		}	break;
+		case _LOGOUT: {
+			// 下线->返回下线信息
+			Logout logout = {};		// 接收包体
+			recv(cli_sockfd, reinterpret_cast<char*>(&logout) + sizeof(DataHeader),
+				sizeof(Login) - sizeof(DataHeader), 0);
+#ifndef Debug
+			printf("包头信息：$cmd: %d, $lenght: %d\n", request_head._cmd, request_head._dataLength);
+			printf("\t$%s已下线.\n", logout._userName);
+#endif
+			// here is 确认退出下线，构造响应包
+			response._cmd = _LOGOUT_RESULT;
+			response._result = true;
+		}	break;
+		default: {
+#ifndef Debug
+			printf("包头信息：$cmd: %d, $lenght: %d\n", request_head._cmd, request_head._dataLength);
+			printf("\t$未定义的命令.\n");
+#endif
+			response._cmd = _ERROR;
+			response._result = false;
+		}
+			break;
+	}
+	// 将结果返回给客户端
+	send(cli_sockfd, reinterpret_cast<const char*>(&response), sizeof(Response), 0);
+	return true;
+}
+
 
 int main() {
 	// 启动windows socket 2.x环境
@@ -88,7 +146,7 @@ int main() {
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(PORT);	// 将主机字节序转换为网络字节序
 	addr.sin_addr.S_un.S_addr = INADDR_ANY;//inet_addr("127.0.0.1");
-	if (bind(sockfd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) == SOCKET_ERROR) {
+	if (bind(sockfd, reinterpret_cast<sockaddr*>(&addr), sizeof(sockaddr_in)) == SOCKET_ERROR) {
 		printf("绑定端口失败!\n");
 	}
 	else {
@@ -100,71 +158,68 @@ int main() {
 	}
 	else {
 		printf("服务器$%d已启动, 正在监听端口：%d...\n", sockfd, PORT);
-		// 配置客户端网络端口地址
-		sockaddr_in cli_addr = {};
-		int addr_len = sizeof(cli_addr);
 		while (true) {
-			// 4.accept 等待客户端连接到来
-			SOCKET cli_sockfd = INVALID_SOCKET;
-			cli_sockfd = accept(sockfd, reinterpret_cast<sockaddr*>(&cli_addr), &addr_len);
-			if (cli_sockfd == INVALID_SOCKET) {
-				printf("接收到无效的客户端SOCKET\n");
+			// 4-1.将服务端套接字添加到描述符结合监控中
+			fd_set reads, writes, excepts;
+			FD_ZERO(&reads);	 FD_ZERO(&writes);	FD_ZERO(&excepts);
+			FD_SET(sockfd, &reads);		FD_SET(sockfd, &writes);	FD_SET(sockfd, &excepts);
+		
+			// 4-2.将通讯套接字加入到监控集合中
+			for (int i = static_cast<int>(g_TalkWithCli_fds.size()) - 1; i >= 0; --i) {
+				FD_SET(g_TalkWithCli_fds[i], &reads);
+				//FD_SET(g_cli_sockfds[i], &writes);
+				//FD_SET(g_cli_sockfds[i], &excepts);
 			}
-			else {
-				printf("新的客户端加入连接...\n\t cli_sockfd = %d && ip = %s.\n",
-					cli_sockfd, inet_ntoa(cli_addr.sin_addr));
-			}
-			while (true) {
-				// 5.recv 接收客户端请求数据包
-				DataHeader request_head = {};	// 接收请求头信息
-				int recv_len = recv(cli_sockfd, reinterpret_cast<char*>(&request_head), sizeof(DataHeader), 0);
-				if (recv_len <= 0) {
-					printf("接收数据失败，客户端退出，任务结束\n");
+
+			timeval timeout = { 3, 0 };	// 最后一个参数表示在timeout=3s内，如果没有可用描述符到来，立即返回
+										// 为0表示阻塞在select，直到有可用描述符到来才返回
+			// 4-3.开始进行监控
+			int ret = select(0, &reads, &writes, &excepts, &timeout);
+			if (ret <= 0) {
+				if (ret == 0) {
+					printf("select 监控到当前无可用描述符就绪.\n");
+					continue;
+				}
+				else {
+					printf("select出错，任务结束.\n");
 					break;
 				}
-
-				// 6.处理客户端请求，解析信息
-				Response response = {};				// 响应
-
-				switch (request_head._cmd) {
-					case _LOGIN: {
-						// 登录->返回登录信息
-						Login login = {};	// 接收包体
-						recv(cli_sockfd, reinterpret_cast<char*>(&login) + sizeof(DataHeader), 
-							sizeof(Login) - sizeof(DataHeader), 0);
-#ifndef Debug
-						printf("包头信息：$cmd: %d, $lenght: %d\n", login._cmd, login._dataLength);
-						printf("\t$%s, $%s已上线.\n", login._userName, login._passwd);
-#endif
-						// here is 判断登录信息，构造响应包
-						response._cmd = _LOGIN_RESULT;
-						response._result = true;
-					}	break;
-					case _LOGOUT: {
-						// 下线->返回下线信息
-						Logout logout = {};		// 接收包体
-						recv(cli_sockfd, reinterpret_cast<char*>(&logout) + sizeof(DataHeader), 
-							sizeof(Login) - sizeof(DataHeader), 0);
-#ifndef Debug
-						printf("包头信息：$cmd: %d, $lenght: %d\n", logout._cmd, logout._dataLength);
-						printf("\t$%s已下线.\n", logout._userName);
-#endif
-						// here is 确认退出下线，构造响应包
-						response._cmd = _LOGOUT_RESULT;
-						response._result = true;
-					}	break;
-					default: {
-						response._cmd = _ERROR;
-						response._result = false;
-					}
-						break;
+			}
+			// 4-4.判断服务端socket是否就绪->客户端有连接到来
+			if (FD_ISSET(sockfd, &reads)) {		
+				FD_CLR(sockfd, &reads);		// 清理监控集合
+				// 4-4-1.accept 等待客户端连接到来，返回一个通讯套接字
+				sockaddr_in cli_addr = {};
+				int addr_len = sizeof(cli_addr);
+				SOCKET cli_sockfd = INVALID_SOCKET;
+				cli_sockfd = accept(sockfd, reinterpret_cast<sockaddr*>(&cli_addr), &addr_len);
+				if (cli_sockfd == INVALID_SOCKET) {
+					printf("接收到无效的客户端SOCKET\n");
 				}
-				// 将结果返回给客户端
-				send(cli_sockfd, reinterpret_cast<const char*>(&response), sizeof(Response), 0);
+				else {
+					// 4-4-2.将新的通讯套接字添加到动态数组中
+					g_TalkWithCli_fds.push_back(cli_sockfd);
+					printf("新的客户端加入连接...\n\t 与其通讯的套接字cli_sockfd = %d && ip = %s.\n",
+						cli_sockfd, inet_ntoa(cli_addr.sin_addr));
+				} 
+			}
+			// 4-5.判断监控集合中是否有剩余描述符->即就绪的通讯描述符
+			for (int i = 0; i < static_cast<int>(reads.fd_count); ++i) {
+				// 4-5-1.如果有即就绪的通讯描述符，对齐进行Porc调用
+				if (Proc(reads.fd_array[i]) == false) {
+					// 4-5-2.Proc调用失败，将通讯描述符从数组中移除
+					auto iterator = find(g_TalkWithCli_fds.begin(), g_TalkWithCli_fds.end(), reads.fd_array[i]);
+					if (iterator != g_TalkWithCli_fds.end()) {
+						g_TalkWithCli_fds.erase(iterator);
+					}
+				}
 			}
 		}
 	}
-	
+	for (int i = g_TalkWithCli_fds.size() - 1; i >= 0; --i) {
+		closesocket(g_TalkWithCli_fds[i]);
+	}
+
 	// 8.关闭套接字 close
 	printf("服务器即将关闭...\n\n");
 	closesocket(sockfd);
